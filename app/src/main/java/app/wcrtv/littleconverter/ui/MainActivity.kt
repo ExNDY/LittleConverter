ec53cb6eb9cc35dcc4ba6dbe9f025f625ac9847c
Konstantin Kolchurin <support@mellman.ru>
2022-01-17 19:30:26 +0700

    base architect

--- app/src/main/java/app/wcrtv/littleconverter/ui/MainActivity.kt
+++ app/src/main/java/app/wcrtv/littleconverter/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package app.wcrtv.littleconverter
+package app.wcrtv.littleconverter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.wcrtv.littleconverter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
